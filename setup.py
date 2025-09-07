97674cfef72ced64dfa803a1018b4bb21a2a6e08
N!no <ninomyemail@gmail.com>
2025-09-07 12:18:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-# setup.py
 from setuptools import find_packages, setup
 
 setup(
     name="comlrl",
-    version="0.1",
+    use_scm_version=True,
+    setup_requires=["setuptools-scm"],
     packages=find_packages(),
     python_requires=">=3.8",
 )
