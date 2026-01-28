20b05adfca6c45c6a34b8a1154b489f5344f39cf
junho328 <jhn9803@gmail.com>
2026-01-28 13:48:54 +0900

    enable flash attn2

--- comlrl/trainers/magrpo.py
+++ comlrl/trainers/magrpo.py
@@ -223,13 +223,24 @@ class MAGRPOTrainer:
             self.num_agents = num_agents
             if isinstance(model, str):
                 from transformers import AutoModelForCausalLM, AutoTokenizer
+                
+                model_load_kwargs = dict(self.model_config.get("model_kwargs", {}))
+                if "attn_implementation" not in model_load_kwargs:
+                    model_load_kwargs["attn_implementation"] = "flash_attention_2"
 
+                # self.agents = [
+                #     AutoModelForCausalLM.from_pretrained(
+                #         model, **self.model_config.get("model_kwargs", {})
+                #     )
+                #     for _ in range(num_agents)
+                # ]
                 self.agents = [
                     AutoModelForCausalLM.from_pretrained(
-                        model, **self.model_config.get("model_kwargs", {})
+                        model, **model_load_kwargs
                     )
                     for _ in range(num_agents)
                 ]
+                
                 self.model_name = model
 
                 if tokenizer is None:
